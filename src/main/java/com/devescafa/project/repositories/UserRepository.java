6ff255dd110cb1fd52b65824c17875ca2fd54130
vescafa <vescafa@hotmail.com>
2025-07-17 14:57:59 -0300

    Service layer, component registration

--- src/main/java/com/devescafa/project/repositories/UserRepository.java
+++ src/main/java/com/devescafa/project/repositories/UserRepository.java
@@ -1,16 +1,9 @@
 package com.devescafa.project.repositories;
 
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devescafa.project.entities.User;
 
-
 public interface UserRepository extends JpaRepository<User, Long> {
-	
-	
-
-	
 
 }
