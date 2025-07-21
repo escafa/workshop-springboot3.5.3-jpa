8c03836413d8438c1ec41f4c45a95796412e7241
vescafa <vescafa@hotmail.com>
2025-07-21 15:46:33 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/devescafa/project/config/TestConfig.java
+++ src/main/java/com/devescafa/project/config/TestConfig.java
@@ -69,8 +69,8 @@ public class TestConfig implements CommandLineRunner {
 		
 		
 		
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u1 = new User(null, "Mopri da Aero", "mopriaero@gmail.com", "969999999", "123456");
+		User u2 = new User(null, "Dz Bowser", "dzbowser@gmail.com", "9244444444", "123456");
 		
 		Order o1 = new Order(null, Instant.parse("2025-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2025-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
